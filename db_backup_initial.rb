c67ac1c2a0beb5cdca38899bb33e1af9291e93d7
miyohide <miyohide@gmail.com>
2014-12-13 20:13:12 +0900

    P5. Using args.

--- db_backup_initial.rb
+++ db_backup_initial.rb
@@ -1,30 +1,16 @@
 #!/usr/bin/env ruby
 
-databases = {
-   big_client: {
-      database: 'big_client',
-      username: 'big',
-      password: 'big',
-   },
-   small_client: {
-      database: 'small_client',
-      username: 'small',
-      password: 'p@ssWord!',
-   }
-}
+database = ARGV.shift
+username = ARGV.shift
+password = ARGV.shift
+end_if_iter = ARGV.shift
 
-end_of_iter = ARGV.shift
-
-databases.each do |name, config|
-   if end_of_iter.nil?
-      backup_file = config[:database] + '_' + Time.now.strftime('%Y%m%d')
-   else
-      backup_file = config[:database] + '_' + end_of_iter
-   end
-   mysqldump = "mysqldump -u#{config[:username]} -p#{config[:password]} " +
-      "#{config[:database]}"
-
-   `#{mysqldump} > #{backup_file}.sql`
-   `gzip #{backup_file}.sql`
+if end_of_iter.nil?
+   backup_file = config[:database] + '_' + Time.now.strftime('%Y%m%d')
+else
+   backup_file = config[:database] + '_' + end_of_iter
 end
+
+`mysqldump -u#{username} -p#{password} #{database} > #{backup_file}.sql`
+`gzip #{backup_file}.sql`
 
